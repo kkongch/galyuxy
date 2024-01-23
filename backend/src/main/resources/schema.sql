a9088fcf3fa946fbc0d6d2bcaca7c130c4dea648
최지웅 <jwchoi42@naver.com>
2024-01-23 17:31:28 +0900

    feat: greeting

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -1,1 +1,8 @@
-create database galyuxy;
+
+-- test
+drop table if exists `between`;
+create table `between` (
+    `id` int not null auto_increment,
+    `name` varchar(10),
+    primary key (`id`)
+);
