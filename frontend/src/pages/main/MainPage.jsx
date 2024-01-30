606713b433fc23db4cf25bda4bfeccec3b9c42e0
오정민 <dhwjdals1324@gmail.com>
2024-01-30 09:51:50 +0900

    메인페이지 css 조정

--- frontend/src/pages/main/MainPage.jsx
+++ frontend/src/pages/main/MainPage.jsx
@@ -4,12 +4,15 @@ import galaxyImage from 'assets/images/Main/갤역시풀네임.png';
 import Background from 'components/Basic/Background';
 import mainbackgroundimage from 'assets/images/Main/메인페이지배경화면.png'
 import heritageImage from 'assets/images/Main/메인_문화유산관람.png'
+import artImage from 'assets/images/Main/메인_미술활동.png'
+import presentationImage from 'assets/images/Main/메인_연극발표활동.png'
+import quizImage from 'assets/images/Main/메인_퀴즈.png'
 
 
 const GalaxyImage = styled.img`
     position: absolute;
     width: 50%;
-    max-width: 1157px;
+    /* max-width: 1157px; */
     height: auto;
     left: 50%;
     top: 30%;
@@ -17,17 +20,20 @@ const GalaxyImage = styled.img`
 `;
 const Frame = styled.img`
   position: absolute;
-  top : 40rem;
-  left : 23.44rem;
+  top : ${props => props.top};
+  left :${props => props.left};
   width : 25rem;
-  height: 31.25rem;
+  height: 36.1875rem;
 `
 
 function MainPage() {
   return (
     <Background backgroundImage={mainbackgroundimage}>
       <GalaxyImage src={galaxyImage} alt="galaxy" style={{ maxWidth: '100%', height: 'auto' }}/>
-      <Frame src={heritageImage} alt='frame'/>
+      <Frame src={heritageImage} alt='frame' top='20vh' left='15vw'/>
+      <Frame src={artImage} alt='frame' top='40vh' left='15vw'/>
+      <Frame src={presentationImage} alt='frame' top='60vh' left='15vw'/>
+      <Frame src={quizImage} alt='frame' top='80vh' left='15vw'/>
     </Background>
   );
 }
