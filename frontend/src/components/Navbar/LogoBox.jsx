621866fc6b9d07a8b1178df269f2f9160a3ebd9d
조형찬 <bd3945@naver.com>
2024-02-01 12:10:23 +0900

    feat : CSS 수정

--- frontend/src/components/Navbar/LogoBox.jsx
+++ frontend/src/components/Navbar/LogoBox.jsx
@@ -1,23 +1,21 @@
 // LogoBox.jsx
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 const StyledLogoBox = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 80%;
-  height: 15%;
+  width: 90%;
+  height: 20%;
   flex-shrink: 0;
   border-radius: 1vw;
   background: blue;
-  box-shadow: 3px 7px 7px 1px rgba(0, 0, 0, 0.25);
-  margin: 5% auto;
-
-
-`;
+  box-shadow: 0.5rem 0.5rem 1rem 0.1rem rgba(0, 0, 0, 0.5);
+  margin: 1rem auto;
+`
 
 const LogoBox = () => {
-  return <StyledLogoBox />;
-};
+  return <StyledLogoBox />
+}
 
-export default LogoBox;
+export default LogoBox
--- frontend/src/components/Navbar/TeacherNav.jsx
+++ frontend/src/components/Navbar/TeacherNav.jsx
@@ -1,12 +1,10 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import Menu from './Menu'; // Menu 컴포넌트 임포트
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import Menu from './Menu' // Menu 컴포넌트 임포트
+import { useNavigate } from 'react-router-dom'
 import navlogo from 'assets/images/갤역시_로고.png'
-import LogoBox from './LogoBox';
-import { BiSolidChevronLeft, BiSolidChevronRight } from "react-icons/bi";
-
-
+import LogoBox from './LogoBox'
+import { BiSolidChevronLeft, BiSolidChevronRight } from 'react-icons/bi'
 
 const NavbarContainer = styled.div`
   height: 100%;
@@ -14,7 +12,7 @@ const NavbarContainer = styled.div`
   position: fixed;
   z-index: 1;
   top: 0;
-  left: ${props => props.isOpen ? '0' : '-25%'};
+  left: ${(props) => (props.isOpen ? '0' : '-25%')};
   background-color: rgba(0, 0, 0, 0.5);
   transition: 0.5s;
   display: flex;
@@ -24,36 +22,35 @@ const NavbarContainer = styled.div`
   border: 1px solid rgba(255, 255, 255, 0.53);
   background: rgba(255, 255, 255, 0.53);
   backdrop-filter: blur(5px);
-`;
+`
 
 // 로고이미지 추가
 const LogoImage = styled.div`
   display: flex;
-  width: 100%; 
+  width: 100%;
   height: auto;
   background-color: var(--logo-bg); // 글로벌 스타일에서 설정할 배경색
   justify-content: center;
   align-items: center;
-  
+
   img {
-    width: 70%; // 이미지의 최대 너비를 100%로 설정
+    width: 60%; // 이미지의 최대 너비를 100%로 설정
     height: 100%; // 이미지의 최대 높이를 100%로 설정
   }
-  `;
-
+`
 
 const ToggleButton = styled.div`
   position: fixed;
   display: flex;
   justify-content: center;
   align-items: center;
-  left: ${props => props.isOpen ? '25%' : '0'};
+  left: ${(props) => (props.isOpen ? '25%' : '0')};
   top: 7%;
   height: 8%;
   width: 3%;
   z-index: 2;
   border-radius: 0px 40% 40% 0px;
-  background: #11235A;
+  background: #11235a;
   color: white;
   cursor: pointer;
   transition: 0.5s;
@@ -62,79 +59,101 @@ const ToggleButton = styled.div`
     font-size: 1000%;
     pointer-events: none;
   }
-`;
-
-
-
+`
 
 // NavbarLink를 별도의 파일로 분리하는 것을 고려필요
 const NavbarLink = styled.a`
-  padding: 0.5vh 3vw;
+  padding: 0.6rem 5rem;
   text-decoration: none;
   color: #545454;
   display: block;
   transition: 0.5s;
-  font-size: 1.3vw;
+  font-size: 150%;
   font-weight: 600;
   line-height: normal;
 
   &:hover {
     color: #f1f1f1;
   }
-`;
+  &:active {
+    color: black;
+  }
+`
 
 const MenuText = styled.div`
-  width: 95%;
+  width: 100%;
 
   hr {
-    border: 0.1rem solid #f1f1f1; 
+    border: 0.2rem solid white;
+    margin: 1rem 3rem 0;
   }
 `
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [openMenu, setOpenMenu] = useState({ art: false, culture: false, theater: false, quiz: false }); // 각 메뉴 항목에 대해 초기 상태 설정
+  const [isOpen, setIsOpen] = useState(false)
+  const [openMenu, setOpenMenu] = useState({
+    art: false,
+    culture: false,
+    theater: false,
+    quiz: false,
+  }) // 각 메뉴 항목에 대해 초기 상태 설정
 
   const toggleNavbar = () => {
-    setIsOpen(!isOpen);
-  };
+    setIsOpen(!isOpen)
+  }
 
   const navigate = useNavigate()
 
   const heritageNavigate = () => {
-    navigate('/heritage');
-  };
-  
+    navigate('/heritage')
+  }
+
   const presentationNavigate = () => {
-    navigate('/presentation');
-  };
+    navigate('/presentation')
+  }
 
   const toggleSubmenu = (menu) => {
-    setOpenMenu({ ...openMenu, [menu]: !openMenu[menu] });
-  };
+    setOpenMenu({ ...openMenu, [menu]: !openMenu[menu] })
+  }
 
   return (
     <>
       <NavbarContainer isOpen={isOpen}>
-      
         <LogoImage>
-          <img src={navlogo} alt="Logo" /> {/* 이미지 경로를 자신의 로고 이미지 경로로 변경하세요. */}
+          <img src={navlogo} alt='Logo' />{' '}
+          {/* 이미지 경로를 자신의 로고 이미지 경로로 변경하세요. */}
         </LogoImage>
         <LogoBox> </LogoBox>
         <MenuText>
-          <Menu title="문화유산 관람" isOpen={openMenu['culture']} onClick={ heritageNavigate }/>
+          <Menu
+            title='문화유산 관람'
+            isOpen={openMenu['culture']}
+            onClick={heritageNavigate}
+          />
           <hr />
-          <Menu title="미술 활동" isOpen={openMenu['art']} onClick={() => toggleSubmenu('art')}>
-            <NavbarLink href="#">드로잉</NavbarLink>
-            <NavbarLink href="#">컬러링북</NavbarLink>
+          <Menu
+            title='미술 활동'
+            isOpen={openMenu['art']}
+            onClick={() => toggleSubmenu('art')}
+          >
+            <NavbarLink href='#'>드로잉</NavbarLink>
+            <NavbarLink href='#'>컬러링북</NavbarLink>
           </Menu>
           <hr />
-          <Menu title="연극 발표 활동" isOpen={openMenu['theater']} onClick={ presentationNavigate }/>
+          <Menu
+            title='연극 발표 활동'
+            isOpen={openMenu['theater']}
+            onClick={presentationNavigate}
+          />
           <hr />
-          <Menu title="퀴즈" isOpen={openMenu['quiz']} onClick={() => toggleSubmenu('quiz')}>
-            <NavbarLink href="#">문제집 목록</NavbarLink>
-            <NavbarLink href="#">문제집 생성</NavbarLink>
-            <NavbarLink href="#">퀴즈 결과</NavbarLink>
+          <Menu
+            title='퀴즈'
+            isOpen={openMenu['quiz']}
+            onClick={() => toggleSubmenu('quiz')}
+          >
+            <NavbarLink href='#'>문제집 목록</NavbarLink>
+            <NavbarLink href='#'>문제집 생성</NavbarLink>
+            <NavbarLink href='#'>퀴즈 결과</NavbarLink>
           </Menu>
         </MenuText>
       </NavbarContainer>
@@ -142,7 +161,7 @@ const Navbar = () => {
         {isOpen ? <BiSolidChevronLeft /> : <BiSolidChevronRight />}
       </ToggleButton>
     </>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
