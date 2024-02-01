f64bfa4686fb3f1808a348b62a84780a28db668f
오정민 <dhwjdals1324@gmail.com>
2024-02-01 17:55:20 +0900

    feat : quiz 메인화면 및 3d모델 css 수정

--- frontend/src/pages/heritage/HeritageList.jsx
+++ frontend/src/pages/heritage/HeritageList.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useRef, useState } from 'react';
-import styled from 'styled-components';
-import Background from 'components/Basic/Background';
-import heritageImage from 'assets/images/Heritage/문화유산메인배경.png';
-import { getHeritage } from 'api/HeritageApi';
+import React, { useEffect, useRef, useState } from 'react'
+import styled from 'styled-components'
+import Background from 'components/Basic/Background'
+import heritageImage from 'assets/images/Heritage/문화유산메인배경.png'
+import { getHeritage } from 'api/HeritageApi'
 const Body = styled.div`
   margin: 0;
   padding: 0;
@@ -18,80 +18,71 @@ const Container = styled.div`
   flex-wrap: wrap;
   justify-content: space-between;
   width: 50vw;
-`;
+`
 
 const StyledbBox = styled.div`
   width: 32%;
   height: 25vh;
-  background-image: url(${props=>props.imageUrl});
-  background-size : cover;
-  border : 2px solid black;
+  background-image: url(${(props) => props.imageUrl});
+  background-size: cover;
+  border: 2px solid black;
   margin: 10vh 1%;
   transform-origin: center;
-`;
-
-
+`
 
 const Box = React.forwardRef((imageData, ref) => {
-  return <StyledbBox ref={ref} imageUrl={imageData}/>;
-});
+  return <StyledbBox ref={ref} imageUrl={imageData} />
+})
 
 function HeritageList() {
-  const [HeritageData,setHeritageData] = useState([]);
+  const [HeritageData, setHeritageData] = useState([])
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await getHeritage();
-        setHeritageData(response.data); // 데이터를 상태에 저장
+        const response = await getHeritage()
+        setHeritageData(response.data) // 데이터를 상태에 저장
       } catch (error) {
-        console.error(error);
+        console.error(error)
       }
-    };
+    }
 
-    fetchData();
-  }, []);
-  const boxRefs = useRef([]);
-  boxRefs.current = [];
+    fetchData()
+  }, [])
+  const boxRefs = useRef([])
+  boxRefs.current = []
 
-  const boxes = HeritageData.heritageImageUrl.map((data,index) => (
-    <Box imageData={data} ref={React.createRef()} key={index}/>
+  const boxes = HeritageData.heritageImageUrl.map((data, index) => (
+    <Box imageData={data} ref={React.createRef()} key={index} />
   ))
-   
-  
 
   useEffect(() => {
     const handleScroll = () => {
       boxRefs.current.forEach((ref, index) => {
-        const box = ref.current;
-        const boxTop = box.getBoundingClientRect().top;
-        let scale = 1;
+        const box = ref.current
+        const boxTop = box.getBoundingClientRect().top
+        let scale = 1
 
         if (boxTop < window.innerHeight) {
-          scale = 1 + boxTop / window.innerHeight;
+          scale = 1 + boxTop / window.innerHeight
         } else {
-          scale = 2;
+          scale = 2
         }
 
-        scale = Math.min(Math.max(scale, 0.5), 2); 
-        box.style.transform = `scale(${scale})`;
-      });
-    };
+        scale = Math.min(Math.max(scale, 0.5), 2)
+        box.style.transform = `scale(${scale})`
+      })
+    }
 
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
   return (
-  
-      <Background backgroundImage={heritageImage}>
-        <Body>
-          <Container>
-            {boxes}
-          </Container>
-        </Body>
-      </Background>
-  );
+    <Background backgroundImage={heritageImage}>
+      <Body>
+        <Container>{boxes}</Container>
+      </Body>
+    </Background>
+  )
 }
 
-export default HeritageList;
-
-
+export default HeritageList
--- frontend/src/pages/quiz/WorkBookList.jsx
+++ frontend/src/pages/quiz/WorkBookList.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
-
+import styled from 'styled-components'
+import Background from 'components/Basic/Background'
+import QuizMainImage from 'assets/images/Quiz/퀴즈메인화면.png'
+const Wrapper = styled.div`
+  position: absolute;
+  width: 65vw;
+  height: 75vh;
+  background-color: #fff9ec;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border-radius: 3.125rem;
+`
+const RoomBox = styled.div`
+  width: 60rem;
+  height: 8.5625rem;
+  display: flex;
+  position: absolute;
+  border-radius: 3.125rem;
+  background-color: #ffca5a;
+`
+const RoomList = styled.div`
+  width: 60vw;
+  height: 70vh;
+  position: absolute;
+  align-items: center;
+  justify-content: center;
+`
 function WorkBookList() {
   return (
-    <div>
-      <h>퀴즈 문제집 목록</h>
-    </div>
+    <Background backgroundImage={QuizMainImage}>
+      <Wrapper>
+        <RoomList>
+          <RoomBox />
+        </RoomList>
+      </Wrapper>
+    </Background>
   )
 }
 
