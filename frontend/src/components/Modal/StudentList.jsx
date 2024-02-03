a80cc54f74037de5ce11279dbd37db5f7e7f70c5
nayeon <kknyapple@gmail.com>
2024-02-03 17:04:14 +0900

    refactor: 불필요한 코드 삭제

--- frontend/src/components/Modal/StudentList.jsx
+++ frontend/src/components/Modal/StudentList.jsx
@@ -1,4 +1,4 @@
-import { React, useEffect } from 'react'
+import { React } from 'react'
 import styled from 'styled-components'
 import { useRecoilState } from 'recoil'
 import { studentListState } from 'Recoil/ClassState'
