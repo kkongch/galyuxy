ed622a7f88b87fbdfac2f756430607afb312374a
nayeon <kknyapple@gmail.com>
2024-02-04 17:13:33 +0900

    fix: 페이지 경로 수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import GlobalStyle from 'styles/GlobalStyle'
-import TeacherNav from 'components/Navbar/TeacherNav'
-import { Routes, Route, Link } from 'react-router-dom'
-import Art from 'pages/art/Art'
-import PresentationMain from 'pages/presentation/PresentationMain'
-import Class from 'pages/class/Class'
-import HeritageList from 'pages/heritage/HeritageList'
-import WorkBookList from 'pages/quiz/WorkBookList'
-import MainPage from 'pages/main/MainPage'
+import React from 'react';
+import GlobalStyle from 'styles/GlobalStyle';
+import TeacherNav from 'components/Navbar/TeacherNav';
+import { Routes, Route, Link } from 'react-router-dom';
+import Art from 'pages/art/Art';
+import PresentationMain from 'pages/presentation/PresentationMain';
+import ClassPage from 'pages/class/ClassPage';
+import HeritageList from 'pages/heritage/HeritageList';
+import WorkBookList from 'pages/quiz/WorkBookList';
+import MainPage from 'pages/main/MainPage';
 
-import PresentationActive from 'pages/presentation/PresentationActive'
-import PresentationRoom from 'pages/presentation/PresentationRoom'
-import Heritage2D from 'pages/heritage/Heritage2D'
-import Heritage3D from 'pages/heritage/Heritage3D'
-import { RecoilRoot } from 'recoil'
+import PresentationActive from 'pages/presentation/PresentationActive';
+import PresentationRoom from 'pages/presentation/PresentationRoom';
+import HeritageDetailPage from 'pages/heritage/HeritageDetailPage';
+import Heritage3D from 'pages/heritage/Heritage3D';
+import { RecoilRoot } from 'recoil';
 
 function App() {
   return (
@@ -24,10 +24,10 @@ function App() {
           <TeacherNav />
           <Routes>
             <Route path='/main' element={<MainPage />} />
-            <Route path='/teacher/class' element={<Class />} />
+            <Route path='/class' element={<ClassPage />} />
             <Route path='/heritage' element={<HeritageList />} />
-            <Route path='/heritage/:id' element={<Heritage2D />} />
-            <Route path='/heritage/3d' element={<Heritage3D />} />
+            <Route path='/heritage/:id' element={<HeritageDetailPage />} />
+            <Route path='/heritage/:id/3d' element={<Heritage3D />} />
             <Route path='/quiz/workbooklist' element={<WorkBookList />} />
             <Route path='/presentation' element={<PresentationMain />} />.
             <Route
@@ -43,7 +43,7 @@ function App() {
         </div>
       </RecoilRoot>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
