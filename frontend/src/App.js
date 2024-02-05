a2c6520b0f9ccd4e8d83faf1840c42f00ed86cc7
kkongch <dhwjdals1324@gmail.com>
2024-02-06 00:50:26 +0900

    feat : 컬러링북 페이지 일부 완성 및 css 수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import GlobalStyle from 'styles/GlobalStyle';
 import TeacherNav from 'components/Navbar/TeacherNav';
-import { Routes, Route, Link } from 'react-router-dom';
-import Art from 'pages/art/Art';
+import { Routes, Route, Link, Router, BrowserRouter} from 'react-router-dom';
+import Coloring from 'pages/art/Coloring';
 import PresentationPage from 'pages/presentation/PresentationPage';
 import ClassPage from 'pages/class/ClassPage';
 import HeritageList from 'pages/heritage/HeritageList';
@@ -20,16 +20,16 @@ function App() {
         <GlobalStyle />
         <div>
           <TeacherNav />
-          <Routes>
-            <Route path='/main' element={<MainPage />} />
-            <Route path='/class' element={<ClassPage />} />
-            <Route path='/heritage' element={<HeritageList />} />
-            <Route path='/heritage/:id' element={<HeritageDetailPage />} />
-            <Route path='/heritage/:id/3d' element={<Heritage3D />} />
-            <Route path='/quiz/workbooklist' element={<WorkBookList />} />
-            <Route path='/presentation' element={<PresentationPage />} />.
-            <Route path='/art' element={<Art />} />
-          </Routes>
+            <Routes>
+              <Route path='/main' element={<MainPage />} />
+              <Route path='/class' element={<ClassPage />} />
+              <Route path='/heritage' element={<HeritageList />} />
+              <Route path='/heritage/:id' element={<HeritageDetailPage />} />
+              <Route path='/heritage/:id/3d' element={<Heritage3D />} />
+              <Route path='/quiz/workbooklist' element={<WorkBookList />} />
+              <Route path='/presentation' element={<PresentationPage />} />.
+              <Route path='/art' element={<Coloring />} />
+            </Routes>
         </div>
       </RecoilRoot>
     </>
