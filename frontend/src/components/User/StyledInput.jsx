f1908c00b8513130d540db075a81966a86ea6bb6
nayeon <kknyapple@gmail.com>
2024-02-05 22:02:43 +0900

    feat: 회원 페이지 input component 제작

---
+++ frontend/src/components/User/StyledInput.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Input = styled.input`
+  padding: 10px;
+  border: 2px solid #c8c8c8;
+  border-radius: 5px;
+  outline: none;
+  width: 35rem;
+  height: 5.3125rem;
+  border-radius: 1.25rem;
+  &:focus {
+    border-color: #596fb7;
+  }
+  &::placeholder {
+    color: #c8c8c8;
+  }
+  font-size: 1.75rem;
+  font-weight: 600;
+`;
+
+const StyledInput = ({ type, id, name, placeholder }) => {
+  return <Input type={type} id={id} name={name} placeholder={placeholder} />;
+};
+
+export default StyledInput;
