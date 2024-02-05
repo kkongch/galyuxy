91f908d2db6efecb9de1c3aa2a7b68b9856d8f95
nayeon <kknyapple@gmail.com>
2024-02-05 11:18:27 +0900

    feat: 활동 목록 CRUD

---
+++ frontend/src/components/Presentation/PresentationModal.jsx
@@ -0,0 +1,155 @@
+import { React, useEffect, useState } from 'react';
+
+import styled from 'styled-components';
+import {
+  isAddModalOpenState,
+  isRefactorModalOpenState,
+} from 'Recoil/ClassState';
+import { useRecoilState } from 'recoil';
+import { presentationListState } from 'Recoil/PresentationState';
+
+const ModalDiv = styled.div`
+  width: 100vw;
+  height: 100%;
+  overflow: hidden;
+  position: absolute;
+  top: 0;
+  background-color: rgba(91, 112, 131, 0.4);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ModalBox = styled.div`
+  width: 93.8125rem;
+  height: 92.125rem;
+  padding: 5rem 9.88rem;
+  background-color: white;
+  border-radius: 3.125rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const ClassNameBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 5.3125rem;
+`;
+const Title = styled.div`
+  text-align: center;
+  font-size: 3rem;
+  font-weight: 600;
+`;
+const LargeInput = styled.input`
+  width: 59.25rem;
+  height: 5.3125rem;
+  border-radius: 1.25rem;
+  border: 2px solid #c8c8c8;
+  font-size: 2rem;
+  font-weight: 600;
+  padding: 0 2.69rem;
+  &:focus {
+    outline: 0.1rem solid #596fb7;
+  }
+`;
+
+const ButtonBox = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  height: 5.125rem;
+  width: 100%;
+`;
+const CancelButton = styled.div`
+  width: 13.54169rem;
+  height: 5.125rem;
+  border-radius: 3.125rem;
+  border-radius: 2.5rem;
+  background: #c8c8c8;
+  color: #fff;
+  font-size: 2rem;
+  font-weight: 600;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 2.52rem;
+  cursor: pointer;
+`;
+
+const ConfirmButton = styled.div`
+  width: 13.54169rem;
+  height: 5.125rem;
+  flex-shrink: 0;
+  background: #596fb7;
+  border-radius: 2.5rem;
+  color: #fff;
+  font-size: 2rem;
+  font-weight: 600;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 2.52rem;
+  cursor: pointer;
+`;
+
+const PresentationModal = ({ presentationId }) => {
+  const [isAddModalOpen, setIsAddModalOpen] =
+    useRecoilState(isAddModalOpenState);
+  const [isRefactorModalOpen, setIsRefactorModalOpen] = useRecoilState(
+    isRefactorModalOpenState
+  );
+  const [presentationTitle, setPresentationTitle] = useState('');
+  const [presentationList, setPresentationList] = useRecoilState(
+    presentationListState
+  );
+
+  useEffect(() => {
+    const presentationWithId = presentationList.find(
+      (item) => item.presentationId === presentationId
+    );
+
+    if (presentationWithId) {
+      setPresentationTitle(presentationWithId.presentationTitle);
+    } else {
+      setPresentationTitle('');
+    }
+  }, []);
+
+  const handleCancel = () => {
+    setIsAddModalOpen(false);
+    setIsRefactorModalOpen(false);
+  };
+
+  const handleConfirm = () => {
+    // PUT /presentation/:presentationId
+
+    setIsAddModalOpen(false);
+    setIsRefactorModalOpen(false);
+  };
+
+  return (
+    <ModalDiv>
+      <ModalBox>
+        <ClassNameBox>
+          <Title>
+            <p>활동 명</p>
+          </Title>
+          <LargeInput
+            value={presentationTitle}
+            onChange={(e) => setPresentationTitle(e.target.value)}
+          />
+        </ClassNameBox>
+        <ButtonBox>
+          <CancelButton onClick={handleCancel}>취소</CancelButton>
+          <ConfirmButton onClick={handleConfirm}>완료</ConfirmButton>
+        </ButtonBox>
+      </ModalBox>
+    </ModalDiv>
+  );
+};
+
+export default PresentationModal;
---
+++ frontend/src/pages/presentation/PresentationPage.jsx
@@ -0,0 +1,129 @@
+import { isAddModalOpenState, userTypeState } from 'Recoil/ClassState';
+import { React } from 'react';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import styled from 'styled-components';
+import mainBackground from 'assets/images/mainBackground.png';
+import Background from 'components/Basic/Background';
+import { presentationListState } from 'Recoil/PresentationState';
+import PresentationList from '../../components/Presentation/PresentationList';
+import PresentationModal from 'components/Presentation/PresentationModal';
+
+const MainBox = styled.main`
+  height: 100vh;
+  width: 100vw;
+`;
+
+const ClassBox = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+`;
+const AddClassButton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 20.5625rem;
+  height: 5.125rem;
+  background-color: #596fb7;
+  color: #fff;
+  text-align: center;
+  font-size: 1.875rem;
+  font-weight: 600;
+  padding: 0 2.31rem;
+  border-radius: 1rem;
+  cursor: pointer;
+`;
+const Text = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 90.1875rem;
+  height: 5.125rem;
+  background-color: #dcab7a;
+  color: #fff;
+  text-align: center;
+  font-size: 1.875rem;
+  font-weight: 600;
+  padding: 0 2.31rem;
+  border-radius: 1rem;
+`;
+const TopOfBoardBox = styled.div`
+  display: flex;
+  width: 90.1875rem;
+  height: 5.125rem;
+  justify-content: flex-end;
+  margin-bottom: 1.19rem;
+`;
+const SvgBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const BoardBackgroundOuter = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 90.1875rem;
+  height: 62.4375rem;
+  background-color: #dcab7a;
+  border-radius: 1.2rem;
+`;
+const BoardBackgroundInner = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  width: 82.1875rem;
+  height: 54.4375rem;
+  background-color: #fccb82;
+  padding: 0 4.3rem;
+`;
+
+const PresentationPage = () => {
+  const userType = useRecoilValue(userTypeState);
+  const [isModalOpen, setIsModalOpen] = useRecoilState(isAddModalOpenState);
+
+  const handleAddClassClick = () => {
+    setIsModalOpen(true);
+  };
+
+  return (
+    <Background backgroundImage={mainBackground}>
+      <MainBox>
+        {isModalOpen && <PresentationModal />}
+        <ClassBox>
+          <TopOfBoardBox>
+            <AddClassButton onClick={handleAddClassClick}>
+              <SvgBox>
+                <svg
+                  width='35'
+                  height='35'
+                  viewBox='0 0 35 35'
+                  fill='none'
+                  xmlns='http://www.w3.org/2000/svg'
+                >
+                  <path
+                    d='M18.9587 10.2077H16.042V16.041H10.2087V18.9577H16.042V24.791H18.9587V18.9577H24.792V16.041H18.9587V10.2077ZM17.5003 2.91602C9.46491 2.91602 2.91699 9.46393 2.91699 17.4993C2.91699 25.5348 9.46491 32.0827 17.5003 32.0827C25.5357 32.0827 32.0837 25.5348 32.0837 17.4993C32.0837 9.46393 25.5357 2.91602 17.5003 2.91602ZM17.5003 29.166C11.0691 29.166 5.83366 23.9306 5.83366 17.4993C5.83366 11.0681 11.0691 5.83268 17.5003 5.83268C23.9316 5.83268 29.167 11.0681 29.167 17.4993C29.167 23.9306 23.9316 29.166 17.5003 29.166Z'
+                    fill='white'
+                  />
+                </svg>
+              </SvgBox>
+
+              <p>활동방 생성</p>
+            </AddClassButton>
+          </TopOfBoardBox>
+
+          <BoardBackgroundOuter>
+            <BoardBackgroundInner>
+              <PresentationList />
+            </BoardBackgroundInner>
+          </BoardBackgroundOuter>
+        </ClassBox>
+      </MainBox>
+    </Background>
+  );
+};
+
+export default PresentationPage;
