366ff46d2f1f4b772dc054103b0d78f6efa44279
nayeon <kknyapple@gmail.com>
2024-02-05 11:27:13 +0900

    refactor: 경로 삭제

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,12 @@ import GlobalStyle from 'styles/GlobalStyle';
 import TeacherNav from 'components/Navbar/TeacherNav';
 import { Routes, Route, Link } from 'react-router-dom';
 import Art from 'pages/art/Art';
-import PresentationMain from 'pages/presentation/PresentationMain';
+import PresentationPage from 'pages/presentation/PresentationPage';
 import ClassPage from 'pages/class/ClassPage';
 import HeritageList from 'pages/heritage/HeritageList';
 import WorkBookList from 'pages/quiz/WorkBookList';
 import MainPage from 'pages/main/MainPage';
 
-import PresentationActive from 'pages/presentation/PresentationActive';
-import PresentationRoom from 'pages/presentation/PresentationRoom';
 import HeritageDetailPage from 'pages/heritage/HeritageDetailPage';
 import Heritage3D from 'pages/heritage/Heritage3D';
 import { RecoilRoot } from 'recoil';
@@ -29,15 +27,7 @@ function App() {
             <Route path='/heritage/:id' element={<HeritageDetailPage />} />
             <Route path='/heritage/:id/3d' element={<Heritage3D />} />
             <Route path='/quiz/workbooklist' element={<WorkBookList />} />
-            <Route path='/presentation' element={<PresentationMain />} />.
-            <Route
-              path='/presentation/:categoryId'
-              element={<PresentationActive />}
-            />
-            <Route
-              path='/presentation/:categoryId/:roomId'
-              element={<PresentationRoom />}
-            />
+            <Route path='/presentation' element={<PresentationPage />} />.
             <Route path='/art' element={<Art />} />
           </Routes>
         </div>
