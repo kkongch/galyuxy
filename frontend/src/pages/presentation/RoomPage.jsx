40bfe778fcc4b726ac2644a0a0d32b40f47512ae
nayeon <kknyapple@gmail.com>
2024-02-05 20:00:30 +0900

    feat: 방 주제 보여주기, 사용자 유형에 따른 화면 전환

--- frontend/src/pages/presentation/RoomPage.jsx
+++ frontend/src/pages/presentation/RoomPage.jsx
@@ -1,6 +1,6 @@
-import { isAddModalOpenState } from 'Recoil/ClassState';
+import { isAddModalOpenState, userTypeState } from 'Recoil/ClassState';
 import { React } from 'react';
-import { useRecoilState } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import styled from 'styled-components';
 import mainBackground from 'assets/images/mainBackground.png';
 import Background from 'components/Basic/Background';
@@ -22,6 +22,11 @@ const ClassBox = styled.div`
   height: 100vh;
   width: 100vw;
 `;
+const ButtonBox = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+`;
 const AddClassButton = styled.div`
   display: flex;
   justify-content: space-between;
@@ -37,26 +42,30 @@ const AddClassButton = styled.div`
   border-radius: 1rem;
   cursor: pointer;
 `;
-const Text = styled.div`
+const StartButton = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 90.1875rem;
+  width: 11.5rem;
   height: 5.125rem;
-  background-color: #dcab7a;
-  color: #fff;
-  text-align: center;
-  font-size: 1.875rem;
+  font-size: 2rem;
   font-weight: 600;
-  padding: 0 2.31rem;
+  background-color: #c8c8c8;
   border-radius: 1rem;
+  cursor: pointer;
+  margin-left: 1.94rem;
 `;
 const TopOfBoardBox = styled.div`
   display: flex;
   width: 90.1875rem;
   height: 5.125rem;
-  justify-content: flex-end;
+  justify-content: space-between;
   margin-bottom: 1.19rem;
+`;
+const Title = styled.div`
+  text-align: center;
+  font-size: 3.75rem;
+  font-weight: 600;
 `;
 const SvgBox = styled.div`
   display: flex;
@@ -79,10 +88,12 @@ const BoardBackgroundOuter = styled.div`
 const RoomPage = () => {
   const [isModalOpen, setIsModalOpen] = useRecoilState(isAddModalOpenState);
   const [roomList, setRoomList] = useRecoilState(roomListState);
+  const userType = useRecoilValue(userTypeState);
 
   useEffect(() => {
-    //GET /room/:presentationId
+    // (student) GET /presentation/:groupId 호출 뒤, active가 1인 presentationId 가져오기
 
+    // (teacher) GET /room/:presentationId
     setRoomList([
       {
         roomScript: '',
@@ -93,6 +104,11 @@ const RoomPage = () => {
         roomScript: '',
         roomId: 2,
         roomSubject: '이순신과 인터뷰하기',
+      },
+      {
+        roomScript: '',
+        roomId: 3,
+        roomSubject: '나의 죽음을 알리지 말라',
       },
     ]);
   }, []);
@@ -107,24 +123,35 @@ const RoomPage = () => {
         {isModalOpen && <RoomModal />}
         <ClassBox>
           <TopOfBoardBox>
-            <AddClassButton onClick={handleAddClassClick}>
-              <SvgBox>
-                <svg
-                  width='35'
-                  height='35'
-                  viewBox='0 0 35 35'
-                  fill='none'
-                  xmlns='http://www.w3.org/2000/svg'
-                >
-                  <path
-                    d='M18.9587 10.2077H16.042V16.041H10.2087V18.9577H16.042V24.791H18.9587V18.9577H24.792V16.041H18.9587V10.2077ZM17.5003 2.91602C9.46491 2.91602 2.91699 9.46393 2.91699 17.4993C2.91699 25.5348 9.46491 32.0827 17.5003 32.0827C25.5357 32.0827 32.0837 25.5348 32.0837 17.4993C32.0837 9.46393 25.5357 2.91602 17.5003 2.91602ZM17.5003 29.166C11.0691 29.166 5.83366 23.9306 5.83366 17.4993C5.83366 11.0681 11.0691 5.83268 17.5003 5.83268C23.9316 5.83268 29.167 11.0681 29.167 17.4993C29.167 23.9306 23.9316 29.166 17.5003 29.166Z'
-                    fill='white'
-                  />
-                </svg>
-              </SvgBox>
-              <p>추가</p>
-            </AddClassButton>
+            <Title>
+              <p>이순신 인터뷰</p>
+            </Title>
+            {userType === 1 && (
+              <ButtonBox>
+                <AddClassButton onClick={handleAddClassClick}>
+                  <SvgBox>
+                    <svg
+                      width='35'
+                      height='35'
+                      viewBox='0 0 35 35'
+                      fill='none'
+                      xmlns='http://www.w3.org/2000/svg'
+                    >
+                      <path
+                        d='M18.9587 10.2077H16.042V16.041H10.2087V18.9577H16.042V24.791H18.9587V18.9577H24.792V16.041H18.9587V10.2077ZM17.5003 2.91602C9.46491 2.91602 2.91699 9.46393 2.91699 17.4993C2.91699 25.5348 9.46491 32.0827 17.5003 32.0827C25.5357 32.0827 32.0837 25.5348 32.0837 17.4993C32.0837 9.46393 25.5357 2.91602 17.5003 2.91602ZM17.5003 29.166C11.0691 29.166 5.83366 23.9306 5.83366 17.4993C5.83366 11.0681 11.0691 5.83268 17.5003 5.83268C23.9316 5.83268 29.167 11.0681 29.167 17.4993C29.167 23.9306 23.9316 29.166 17.5003 29.166Z'
+                        fill='white'
+                      />
+                    </svg>
+                  </SvgBox>
+                  <p>추가</p>
+                </AddClassButton>
+                <StartButton>
+                  <p>시작</p>
+                </StartButton>
+              </ButtonBox>
+            )}
           </TopOfBoardBox>
+
           <BoardBackgroundOuter>
             <RoomList />
           </BoardBackgroundOuter>
