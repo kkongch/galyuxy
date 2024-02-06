fc56270d301dd3e6f9a6c78231af0b31b54574ff
김현영 <hggew@naver.com>
2024-02-06 17:47:18 +0900

    feat: 그룹 수정, 삭제 메서드 추가

--- backend/src/main/java/com/ssafy/domain/classroom/controller/GroupController.java
+++ backend/src/main/java/com/ssafy/domain/classroom/controller/GroupController.java
@@ -8,6 +8,7 @@ import com.ssafy.domain.heritage.Dto.ArtworkResultDto;
 import com.ssafy.global.common.dto.Message;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -51,6 +52,26 @@ public class GroupController {
         return ResponseEntity.ok().body(Message.success(studentDtoList));
     }
 
+    // 그룹 및 그룹 내 학생 리스트 수정 + 추가
+    @PutMapping("/{groupId}")
+    @PreAuthorize("hasAuthority('TEACHER')")
+    ResponseEntity<Message<Void>> putGroupAndStudents(@AuthenticationPrincipal TeacherLoginActiveDto teacherLoginActiveDto,
+                                                       @RequestBody GroupStudentsDto request) {
+
+        //그룹 이름 업데이트
+        Group group = groupService.updateName(request.getGroup());
+        if(group == null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Message.fail(String.valueOf(HttpStatus.NOT_FOUND), "해당 그룹이 존재하지 않음"));
+        }
+        
+        //그룹id로 기존 학생 리스트 가져오기
+        studentService.getStudentListByGroupId(request.getGroup().getId() );
+        // id가 있는 학생과 기존 학생 비교해서 삭제된 기존 학생 찾기
+        
+        // id가 없는 학생 찾아서 추가하기
+        
+        return ResponseEntity.ok().body(Message.success());
+    }
 
 
 }
