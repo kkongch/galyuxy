b3c422116500572232fa209753506a61b4fb1db4
오정민 <dhwjdals1324@gmail.com>
2024-02-14 14:56:58 +0900

    feat : quizdetailteacher api 붙이기 완료

--- frontend/src/api/HeritageApi.js
+++ frontend/src/api/HeritageApi.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
-const baseURL = process.env.REACT_APP_BASE_URL;
 
+const baseURL = process.env.REACT_APP_BASE_URL;
+console.log(baseURL);
 const api = axios.create({
   baseURL,
 });
+
 export async function getHeritage() {
   try {
-    const response = await axios.get('/heritage');
+    const response = await api.get('/heritage');
     // const response = await axios.get(`http://localhost:8080/api/heritage`);
     return response;
   } catch (e) {
@@ -16,8 +18,10 @@ export async function getHeritage() {
 
 export const getHeritageList = async (presentationId) => {
   try {
-    const response = await axios.get(`/heritage`);
-    // const response = await axios.get(`http://localhost:8080/api/heritage`);
+    const response = await api.get('/heritage');
+    // const response = await axios.get(
+    //   `https://i10c206.p.ssafy.io:8080/heritage`
+    // );
     return response.data;
   } catch (error) {
     console.error('Error getHeritageList: ', error);
@@ -28,7 +32,7 @@ export const getHeritageList = async (presentationId) => {
 export const getEraList = async () => {
   try {
     // const response = await axios.get(`http://i10c206.p.ssafy.io:8080/era`);
-    const response = await axios.get(`/era`);
+    const response = await api.get(`/era`);
     return response.data.dataBody;
   } catch (error) {
     console.error('Error getEraList: ', error);
