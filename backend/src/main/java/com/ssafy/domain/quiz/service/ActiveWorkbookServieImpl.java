e8430bf3a5689432c51b022898ce3374674869ba
김현영 <hggew@naver.com>
2024-02-14 21:42:38 +0900

    feat: activework 관련 주석
    Former-commit-id: 7637750cee7feaac5450395e12f2b485b64c668b

--- backend/src/main/java/com/ssafy/domain/quiz/service/ActiveWorkbookServieImpl.java
+++ backend/src/main/java/com/ssafy/domain/quiz/service/ActiveWorkbookServieImpl.java
@@ -1,63 +1,63 @@
-package com.ssafy.domain.quiz.service;
-
-import com.ssafy.domain.classroom.entity.Group;
-import com.ssafy.domain.classroom.repository.GroupRepository;
-import com.ssafy.domain.quiz.dto.ActiveWorkbookDTO;
-import com.ssafy.domain.quiz.dto.ActiveWorkbookProjection;
-import com.ssafy.domain.quiz.entity.ActiveWorkbook;
-import com.ssafy.domain.quiz.entity.Workbook;
-import com.ssafy.domain.quiz.repository.ActiveWorkbookRepository;
-import com.ssafy.domain.quiz.repository.WorkbookRepository;
-import jakarta.persistence.EntityNotFoundException;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Optional;
-
-@Service
-@RequiredArgsConstructor
-public class ActiveWorkbookServieImpl implements ActiveWorkbookService {
-
-    private final GroupRepository groupRepository;
-    private final WorkbookRepository workbookRepository;
-    private final ActiveWorkbookRepository activeWorkbookRepository;
-
-    @Override
-    public ActiveWorkbookDTO putOne(ActiveWorkbookDTO activeWorkbookDTO) {
-
-        Integer groupId = activeWorkbookDTO.getGroupId();
-        Group group = groupRepository.findById(groupId)
-                .orElseThrow(() -> new EntityNotFoundException(String.format("Group %d Not Found", groupId)));
-
-        Integer workbookId = activeWorkbookDTO.getWorkbookId();
-        Workbook workbook = workbookRepository.findById(workbookId)
-                .orElseThrow(() -> new EntityNotFoundException(String.format("Workbook %d Not Found", workbookId)));
-
-        List<ActiveWorkbookProjection> existedActiveWorkbookDTOList = activeWorkbookRepository.findActiveWorkbookByGroupId(groupId);
-        if (existedActiveWorkbookDTOList.size() >= 1) {
-            ActiveWorkbookProjection existedActiveWorkbookProjection = existedActiveWorkbookDTOList.get(0);
-            Optional<ActiveWorkbook> optionalActiveWorkbook = activeWorkbookRepository.findById(existedActiveWorkbookProjection.getActiveWorkbookId());
-            ActiveWorkbook activeWorkbook = optionalActiveWorkbook.get();
-            activeWorkbook.setStart(activeWorkbookDTO.getActiveWorkbookStart());
-            activeWorkbook.setEnd(activeWorkbookDTO.getActiveWorkbookEnd());
-            activeWorkbook.setWorkbook(workbook);
-            activeWorkbookRepository.save(activeWorkbook);
-        } else {
-            ActiveWorkbook activeWorkbook = ActiveWorkbook.builder()
-                    .start(activeWorkbookDTO.getActiveWorkbookStart())
-                    .end(activeWorkbookDTO.getActiveWorkbookEnd())
-                    .group(group)
-                    .workbook(workbook)
-                    .build();
-            activeWorkbookRepository.save(activeWorkbook);
-        }
-        return activeWorkbookDTO;
-    }
-
-    @Override
-    public List<ActiveWorkbookProjection> findActiveWorkbookByGroupId(Integer groupId) {
-        return activeWorkbookRepository.findActiveWorkbookByGroupId(groupId);
-    }
-
-}
+//package com.ssafy.domain.quiz.service;
+//
+//import com.ssafy.domain.classroom.entity.Group;
+//import com.ssafy.domain.classroom.repository.GroupRepository;
+//import com.ssafy.domain.quiz.dto.ActiveWorkbookDTO;
+//import com.ssafy.domain.quiz.dto.ActiveWorkbookProjection;
+//import com.ssafy.domain.quiz.entity.ActiveWorkbook;
+//import com.ssafy.domain.quiz.entity.Workbook;
+//import com.ssafy.domain.quiz.repository.ActiveWorkbookRepository;
+//import com.ssafy.domain.quiz.repository.WorkbookRepository;
+//import jakarta.persistence.EntityNotFoundException;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class ActiveWorkbookServieImpl implements ActiveWorkbookService {
+//
+//    private final GroupRepository groupRepository;
+//    private final WorkbookRepository workbookRepository;
+//    private final ActiveWorkbookRepository activeWorkbookRepository;
+//
+//    @Override
+//    public ActiveWorkbookDTO putOne(ActiveWorkbookDTO activeWorkbookDTO) {
+//
+//        Integer groupId = activeWorkbookDTO.getGroupId();
+//        Group group = groupRepository.findById(groupId)
+//                .orElseThrow(() -> new EntityNotFoundException(String.format("Group %d Not Found", groupId)));
+//
+//        Integer workbookId = activeWorkbookDTO.getWorkbookId();
+//        Workbook workbook = workbookRepository.findById(workbookId)
+//                .orElseThrow(() -> new EntityNotFoundException(String.format("Workbook %d Not Found", workbookId)));
+//
+//        List<ActiveWorkbookProjection> existedActiveWorkbookDTOList = activeWorkbookRepository.findActiveWorkbookByGroupId(groupId);
+//        if (existedActiveWorkbookDTOList.size() >= 1) {
+//            ActiveWorkbookProjection existedActiveWorkbookProjection = existedActiveWorkbookDTOList.get(0);
+//            Optional<ActiveWorkbook> optionalActiveWorkbook = activeWorkbookRepository.findById(existedActiveWorkbookProjection.getActiveWorkbookId());
+//            ActiveWorkbook activeWorkbook = optionalActiveWorkbook.get();
+//            activeWorkbook.setStart(activeWorkbookDTO.getActiveWorkbookStart());
+//            activeWorkbook.setEnd(activeWorkbookDTO.getActiveWorkbookEnd());
+//            activeWorkbook.setWorkbook(workbook);
+//            activeWorkbookRepository.save(activeWorkbook);
+//        } else {
+//            ActiveWorkbook activeWorkbook = ActiveWorkbook.builder()
+//                    .start(activeWorkbookDTO.getActiveWorkbookStart())
+//                    .end(activeWorkbookDTO.getActiveWorkbookEnd())
+//                    .group(group)
+//                    .workbook(workbook)
+//                    .build();
+//            activeWorkbookRepository.save(activeWorkbook);
+//        }
+//        return activeWorkbookDTO;
+//    }
+//
+//    @Override
+//    public List<ActiveWorkbookProjection> findActiveWorkbookByGroupId(Integer groupId) {
+//        return activeWorkbookRepository.findActiveWorkbookByGroupId(groupId);
+//    }
+//
+//}
