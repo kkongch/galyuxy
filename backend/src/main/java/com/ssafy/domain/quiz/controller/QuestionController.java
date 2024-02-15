4a8653e3f26f9a967a70969a5d846549ac63d90d
최지웅 <jwchoi42@naver.com>
2024-02-15 10:29:37 +0900

    remove: http로 수정, local에서 CORS 나오지 않게 CrossOrigin 추가
    Former-commit-id: f4dcf1a9d51e7073b721afef86b6e36fe922fd9e

--- backend/src/main/java/com/ssafy/domain/quiz/controller/QuestionController.java
+++ backend/src/main/java/com/ssafy/domain/quiz/controller/QuestionController.java
@@ -17,6 +17,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/quiz/question")
 @RequiredArgsConstructor
